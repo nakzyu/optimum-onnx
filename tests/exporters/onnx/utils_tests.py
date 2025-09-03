03d4de384ffd4a219752b9b3a25b32a22066af2c
simondanielsson <simon.danielsson99@hotmail.com>
2025-09-03 16:20:51 +0200

    Add gemma3 tests

--- tests/exporters/onnx/utils_tests.py
+++ tests/exporters/onnx/utils_tests.py
@@ -103,6 +103,7 @@ PYTORCH_EXPORT_MODELS_TINY = {
     },
     "flaubert": "hf-internal-testing/tiny-random-flaubert",
     "gemma": "fxmarty/tiny-random-GemmaForCausalLM",
+    "gemma3": "hf-internal-testing/tiny-random-Gemma3ForConditionalGeneration",
     "glpn": "hf-internal-testing/tiny-random-GLPNModel",
     "gpt2": "hf-internal-testing/tiny-random-gpt2",
     "gpt_bigcode": "hf-internal-testing/tiny-random-GPTBigCodeModel",
